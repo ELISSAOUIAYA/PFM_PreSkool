965d3c96ea4467080a9ddf4cc3b2ea762ee66389
ELISSAOUIAYA <ayaelissaoui002@gmail.com>
2026-03-26 20:12:28 +0100

    Fin Intégration Dashboard et Modèles Student

--- student/views.py
+++ student/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def student_list(request):
+    return HttpResponse("Page de liste des étudiants (Bientôt disponible)")
+
+def add_student(request):
+    return HttpResponse("Page d'ajout d'étudiant (Bientôt disponible)")
